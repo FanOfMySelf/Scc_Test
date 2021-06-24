86f78bc9e550d993a4da5f93c8d75eac8cdcfba9
FanOfMySelf <bibo2178@gmail.com>
2021-06-25 03:48:41 +0700

    Stuck at KO binding

--- ProductOrder/Models/ProductRepository.cs
+++ ProductOrder/Models/ProductRepository.cs
@@ -14,10 +14,11 @@ namespace ProductOrder.Models
         public ProductRepository()
         {
             // Add products for the Demonstration
-            Add(new ProductOrderList { Order_No = 3, Supplier = "Electronics", Stock_Site = "ss123ss", Stock_Name="zxczxc", Order_Date = DateTime.UtcNow, Last_Update=DateTime.Now });
+            Add(new ProductOrderList { Order_No = 3, Supplier = "Electronics", Stock_Site = "ss123ss", Stock_Name = "zxczxc", Order_Date = DateTime.Now, Last_Update = DateTime.Now });
             Add(new ProductOrderList { Order_No = 2, Supplier = "Electrasdasdonics", Stock_Site = "ssasdss", Stock_Name = "zxsdczxc", Order_Date = DateTime.UtcNow, Last_Update = DateTime.Now });
             Add(new ProductOrderList { Order_No = 1, Supplier = "Electxxxzzzzronics", Stock_Site = "ssss", Stock_Name = "zxcsdzxc", Order_Date = DateTime.UtcNow, Last_Update = DateTime.Now });
         }
+
 
         public ProductOrderList Add(ProductOrderList item)
         {
--- ProductOrder/Scripts/POlist.js
+++ ProductOrder/Scripts/POlist.js
@@ -5,13 +5,14 @@ function ProductOrderViewModel() {
     //Make the self as 'this' reference
     var self = this;
 
-    self.Order_No = ko.observable("");
+    self.Order_No = ko.observable();
     self.Supplier = ko.observable("");
     self.Stock_Site = ko.observable("");
     self.Stock_Name = ko.observable("");
-    self.Order_Date = ko.observable("");
-    self.Last_Update = ko.observable(""); 
+    self.Order_Date = ko.observable();
+    self.Last_Update = ko.observable(); 
     
+  
 
     var Product = function (Order_No, Supplier, Stock_Site, Stock_Name, Order_Date, Last_Update)
     {
@@ -19,33 +20,40 @@ function ProductOrderViewModel() {
         self.Supplier = ko.observable(Supplier)
         self.Stock_Site = ko.observable(Stock_Site)
         self.Stock_Name = ko.observable(Stock_Name)
-        self.Order_Date = ko.observable(Order_Date)
-        self.Last_Update = ko.observable(Last_Update)
+        self.Order_Date = Date()
+        self.Last_Update = Date()
     };
 
     self.Product = ko.observable();
     self.Products = ko.observableArray(); // Contains the list of products
-    this.Productss = ko.observableArray();
+    self.Productss = ko.observableArray();
+
     
 
     $.ajax({
-        //url: '@Url.Action("GetData", "ProductOrder")',
-        async :false ,  
+        //url: '@Url.Action("GetData", "ProductOrder" )',
+        //async :false ,  
         url: MyAppUrlSettings.MyUsefulUrl,
         cache: false,
         type: 'GET',
+        datatype: "json" ,
         contentType: 'application/json; charset=utf-8',
         data: {},
         success: function (data) {
-            self.Products(data); 
-            console.log(self.Products());
+            
+            self.Products(data);
+            //console.log((data));
             
         }
     });
    // this.Productss=self.Products()
-   //console.log(self.Products());
-    //self.Products.push(new Product(data[i].Order_No, data[i].Supplier, data[i].Stock_Site, data[i].Stock_Name, data[i].Order_Date, data[i].Last_Update))
-    
+    console.log(this.Productss());
+    for (var i = 0; i < self.Products().length; i++)
+    {
+       
+        //this.Productss.push(new Product(self.Products()[i].Order_No, self.Products()[i].Supplier, self.Products()[i].Stock_Site, self.Products()[i].Stock_Name, self.Products()[i].Order_Date, self.Products()[i].Last_Update))
+    }
+    //console.log(this.Productss());
 }
 var viewModel = new ProductOrderViewModel();
 ko.applyBindings(viewModel);
